65281241c86322f5473893ffdc0c60bd513bee7b
lebbs <35425109+lebbs@users.noreply.github.com>
2020-09-04 09:42:22 +0300

    Backup

--- 2020 xr testi/Assets/ActivateNextTrigger.cs
+++ 2020 xr testi/Assets/ActivateNextTrigger.cs
@@ -8,7 +8,9 @@ public class ActivateNextTrigger : MonoBehaviour
     public GameObject rightTrigger;
     public GameObject leftTrigger;
     public GameObject centerTrigger;
-    public GameObject Uitext;
+    public GameObject Uitext1;
+    public GameObject Uitext2;
+    public GameObject Uitext3;
     Rigidbody rigidbody;
 
     
@@ -18,36 +20,55 @@ public class ActivateNextTrigger : MonoBehaviour
    
 
     // Start is called before the first frame update
-    private IEnumerator OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
 
-        if (other.gameObject.CompareTag("RedCube"))
-
-            Uitext.SetActive(true);
-        yield return new WaitForSeconds(3);
+        if (other.gameObject.CompareTag("RightTrigger"))
         {
-                
-                rightTrigger.SetActive(false);
-                leftTrigger.SetActive(true);
-            }
 
-
-        if (other.gameObject.CompareTag("RedCube") && leftTrigger.activeSelf)
-            Uitext.SetActive(true);
-
-        yield return new WaitForSeconds(3);
-            {
-                leftTrigger.SetActive(false);
-                centerTrigger.SetActive(true);
-            }
-
-        if(other.gameObject.CompareTag("RedCube") && centerTrigger.activeSelf)
-            Uitext.SetActive(true);
-        yield return new WaitForSeconds(3);
-        {
-            centerTrigger.SetActive(false);
-            
+            Uitext1.SetActive(true);
+            StartCoroutine(RightTrigger());
+           // new WaitForSeconds(3);
+            //ActivateRight();
         }
+
+        if (other.gameObject.CompareTag("LeftTrigger"))
+        {
+            Uitext2.SetActive(true);
+            //yield return new WaitForSeconds(2);    
+            StartCoroutine(LeftTrigger());
+        }
+
+        if (other.gameObject.CompareTag("CenterTrigger"))
+        {
+
+            Uitext3.SetActive(true);
+            StartCoroutine(CenterTrigger());
+            
+        }  
+        
+    }
+
+    
+
+    IEnumerator RightTrigger()
+    {
+        yield return new WaitForSeconds(2);
+        rightTrigger.SetActive(false);
+        leftTrigger.SetActive(true);
+    }
+
+    IEnumerator LeftTrigger()
+    {
+        yield return new WaitForSeconds(2);
+        leftTrigger.SetActive(false);
+        centerTrigger.SetActive(true);
+    }
+
+    IEnumerator CenterTrigger()
+    {
+        yield return new WaitForSeconds(2);
+        centerTrigger.SetActive(false);
     }
 
 }
